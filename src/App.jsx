55569e784b076473939bbc8dd6ca036f98a767c7
Anthony Romaine <anthony.romaine1@gmail.com>
2023-03-05 14:58:26 -0800

    Create Navbar component with export/download button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Navbar from "./components/Navbar/Navbar";
+
 export default function App() {
   return (
     <div>
-      <h1 className='font-bold'>App</h1>
+      {/* Navbar */}
+      <Navbar />
+      {/* SidePanel */}
+      {/* PreviewPanel */}
     </div>
   )
 }
