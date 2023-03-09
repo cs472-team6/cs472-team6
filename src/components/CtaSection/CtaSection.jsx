db7df7e24ed39137cbd44a9bb906960a46cfbd47
WuliuQ <doubixian@gmail.com>
2023-03-09 12:53:51 -0800

    Update CtaSection.jsx
    update ctasection.jsx

--- src/components/CtaSection/CtaSection.jsx
+++ src/components/CtaSection/CtaSection.jsx
@@ -1,12 +1,14 @@
 import './GradientText.css';
 
 
-export default function CtaSection({options}) {
+export default function CtaSection() {
+  const link = "www.google.com"
+  const word = "Let's Work Together"
   return (
     <div className= "cta-section">
       <div style={{ textAlign: "left" }}>
       <h2>See the impact of good, conversion-oriented design on your business.</h2> 
-      <a href="www.google.com" style={{fontSize: '18px', fontWeight: 'bold' }}><span className="gradient-link">Ler's Work Together</span></a>
+      <a href={link} style={{fontSize: '18px', fontWeight: 'bold' }}><span className="gradient-link">{word}</span></a>
       </div>
     </div>
   );
