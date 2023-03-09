bdb7365cc2402de0f8a1d16f6d5eaf89f8f091a6
WuliuQ <doubixian@gmail.com>
2023-03-09 13:06:02 -0800

    Update CtaSection.jsx

--- src/components/CtaSection/CtaSection.jsx
+++ src/components/CtaSection/CtaSection.jsx
@@ -6,7 +6,7 @@ export default function CtaSection() {
   const word = "Let's Work Together"
   return (
     <div className= "cta-section">
-      <div style={{ textAlign: "left" }}>
+      <div style={{ textAlign: "left", fontSize: '18px', fontWeight: 'bold'}}>
       <h2>See the impact of good, conversion-oriented design on your business.</h2> 
       <a href={link} style={{fontSize: '18px', fontWeight: 'bold' }}><span className="gradient-link">{word}</span></a>
       </div>
