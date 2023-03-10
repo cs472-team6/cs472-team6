3dae964e035364a201a5569114495cc8e513c30d
WuliuQ <doubixian@gmail.com>
2023-03-09 18:18:19 -0800

    Update files

--- src/components/CtaSection/CtaSectionOptions.jsx
+++ src/components/CtaSection/CtaSectionOptions.jsx
@@ -1,19 +1,36 @@
-export default function CtaSectionOptions({options, updateComponent}) {
-    let {link, word1, word2} = options;
-
+export default function CtaSectionOptions({ options, updateComponent }) {
+    let { link, word1, word2 } = options;
+  
     function handleChange(e) {
-        updateComponent({ [e.target.name]: e.target.value});
+      updateComponent({ [e.target.name]: e.target.value });
     }
-
-    
+  
     return (
-        <div className="p-3">
-            <label className="block">Link:</label>
-            <input className={`block mb-3 p-2 rounded-lg bg-gray-200`} type="text" name="link" value={link} onChange={(handleChange)} />
-            <label className="block">Word1:</label>
-            <input className={`block mb-3 p-2 rounded-lg bg-gray-200`} type="text" name="word1" value={word1} onChange={(handleChange)} />
-            <label className="block">Word2:</label>
-            <input className={`block mb-3 p-2 rounded-lg bg-gray-200`} type="text" name="word2" value={word2} onChange={(handleChange)} />
-        </div>
-    )
-}
+      <div className="p-3">
+        <label className="block">Link:</label>
+        <input
+          className={`block mb-3 p-2 rounded-lg bg-gray-200`}
+          type="text"
+          name="link"
+          value={link}
+          onChange={handleChange}
+        />
+        <label className="block">Word1:</label>
+        <input
+          className={`block mb-3 p-2 rounded-lg bg-gray-200`}
+          type="text"
+          name="word1"
+          value={word1}
+          onChange={handleChange}
+        />
+        <label className="block">Word2:</label>
+        <input
+          className={`block mb-3 p-2 rounded-lg bg-gray-200`}
+          type="text"
+          name="word2"
+          value={word2}
+          onChange={handleChange}
+        />
+      </div>
+    );
+  }
--- src/utils/registeredComponents.js
+++ src/utils/registeredComponents.js
@@ -3,7 +3,11 @@ import BannerOptions from "../components/Banner/BannerOptions";
 import BannerPreview from "../components/Banner/BannerPreview.png";
 import Component from "../models/Component";
 import { Gradients } from "./utils";
+import CtaSection from "../components/CtaSection/CtaSection";
+import CtaSectionOptions from "../components/CtaSection/CtaSectionOptions";
+import CtaSectionPreview from "../components/CtaSection/CtaSectionPreview.png"
 
 export const registeredComponents = new Map([
     ['Banner', new Component('Banner', Banner, BannerOptions, BannerPreview, {words: 'Design,Develop,Discover', gradient: Gradients.default})]
+    ['CtaSection', new Component('CtaSection', CtaSection, CtaSectionOptions, CtaSectionPreview, {link: 'www.google.com', word1: 'See the impoact of good, conversion-oriented design on your business.', word2: "Let's Work Together"})]
 ]);
