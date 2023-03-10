44bff1ca9c0f2b3754a9b916e0da6ec7b10aad38
WuliuQ <doubixian@gmail.com>
2023-03-09 17:58:57 -0800

    add file
    add CtaSectionOptions.jsx and CtaSectionPreview.png

--- src/components/CtaSection/CtaSection.jsx
+++ src/components/CtaSection/CtaSection.jsx
@@ -1,14 +1,14 @@
 import './GradientText.css';
 
 
-export default function CtaSection() {
-  const link = "www.google.com"
-  const word = "Let's Work Together"
+export default function CtaSection({options}) {
+  const {link, word1, word2} = options;
+  
   return (
     <div className= "cta-section">
       <div style={{ textAlign: "left", fontSize: '18px', fontWeight: 'bold', display: 'flex', flexDirection: 'column', width: '100%', height: '100%',flexGrow: 1}}>
-      <h2>See the impact of good, conversion-oriented design on your business.</h2> 
-      <a href={link} style={{fontSize: '18px', fontWeight: 'bold' }}><span className="gradient-link">{word}</span></a>
+      <h2>{word1}</h2> 
+      <a href={link} style={{fontSize: '18px', fontWeight: 'bold' }}><span className="gradient-link">{word2}</span></a>
       </div>
     </div>
   );
