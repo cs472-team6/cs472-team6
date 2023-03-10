af0a94ad6287ef24db010bc92e34493df55bee4d
WuliuQ <doubixian@gmail.com>
2023-03-09 18:56:32 -0800

    Update registeredComponents.js

--- src/utils/registeredComponents.js
+++ src/utils/registeredComponents.js
@@ -9,5 +9,4 @@ import CtaSectionPreview from "../components/CtaSection/CtaSectionPreview.png"
 
 export const registeredComponents = new Map([
     ['Banner', new Component('Banner', Banner, BannerOptions, BannerPreview, {words: 'Design,Develop,Discover', gradient: Gradients.default})]
-    ['CtaSection', new Component('CtaSection', CtaSection, CtaSectionOptions, CtaSectionPreview, {link: 'www.google.com', word1: 'See the impoact of good, conversion-oriented design on your business.', word2: "Let's Work Together"})]
 ]);
